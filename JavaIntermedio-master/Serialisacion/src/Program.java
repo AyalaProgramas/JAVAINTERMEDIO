2b1a4b953326bb0a8b08bbd10105db4c7e5a4772
AyalaProgramas <31025807+AyalaProgramas@users.noreply.github.com>
2018-11-27 15:30:35 -0300

    Add files via upload

--- JavaIntermedio-master/Serialisacion/src/Program.java
+++ JavaIntermedio-master/Serialisacion/src/Program.java
@@ -1,35 +1,86 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 
-import com.sun.java_cup.internal.runtime.Scanner;
+//import com.sun.java_cup.internal.runtime.Scanner;
+
+
 
 public class Program {
 	
-	static Scanner  s=new Scanner(System.in); 
+	 static Scanner  sc=new Scanner(System.in); 
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException {
 			String g ="este texto lo voy a guardar en el disco rigido";
-			try
+			int opcion;
+			
+			
+			
+			ImprimirMenu();
+			opcion=sc.nextInt();
+			sc.nextLine();
+			
+			switch(opcion)
 			{
-				OutputStream fos=new FileOutputStream("C:/Users/Alumno/Desktop/archivouno.fafa");
-				ObjectOutputStream oos=new ObjectOutputStream(fos);
-				oos.writeObject(g);
+			case 1:
+				System.out.println("ingresar dato");
+				g=sc.nextLine();
+
+				try
+				{
+					
+					OutputStream fos=new FileOutputStream("C:/Users/lenovo/Desktop/prueba/sasasa.txt");
+					ObjectOutputStream oos=new ObjectOutputStream(fos);
+					oos.writeObject(g);
+					
+				}catch(FileNotFoundException e)
+				{
+					e.printStackTrace();
+				}
+				catch(IOException e)
+				{
+					e.printStackTrace();
+				}
+				finally
+				{
+					
+				}
 				
-			}catch(FileNotFoundException e)
-			{
-				e.printStackTrace();
-			}
-			catch(IOException e)
-			{
-				e.printStackTrace();
-			}
-			finally
-			{
+				break;
+			case 2:
 				
+				
+				break;
+			case 3:
+				try
+				{
+					String recupero;
+				ObjectInputStream ios=new ObjectInputStream(new FileInputStream("C:/Users/lenovo/Desktop/prueba/sasasa.txt"));
+					recupero=(String)ios.readObject();
+					//g=(String)ios.readObject();
+					System.out.println(recupero);
+				}
+				catch(FileNotFoundException e)
+				{
+					e.printStackTrace();
+				}
+				catch(IOException e)
+				{
+					e.printStackTrace();
+				}
+				break;
+			case 4:
+				break;
+				
+			
 			}
+			
+			
 			
 			
 	}
@@ -37,9 +88,9 @@ public class Program {
 	public static void ImprimirMenu()
 	{
 		System.out.println("escribir");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
+		System.out.println("guardar");
+		System.out.println("cargar");
+		System.out.println("imprimir");
 	}
 
 }
